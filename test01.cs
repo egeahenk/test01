327fa0cc6df364676e11d83ea41d63c9a2b11443
Ege <egemenwho@gmail.com>
2023-07-22 23:49:25 +0300

    test02

--- test01.cs
+++ test01.cs
@@ -7,7 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        
+            Console.WriteLine("Test01");
+            Console.ReadLine();
 
+            Console.WriteLine("Test02");
             Console.ReadLine();
         }
     }
